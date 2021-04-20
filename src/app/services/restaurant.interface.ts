89cd2bf6586c780e9166c1f0272efc545ad47435
damia.smith <damia.v.smith@gmail.com>
2021-04-20 18:44:40 -0500

    finish update

--- src/app/services/restaurant.interface.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Restaurant {
-    restaurantName: string;
-    address: Array <string>;
-    phoneNumber: string;
-    hours: Array <string>;
-    website: string;
-    foodItems?: Array<object>;
-}
